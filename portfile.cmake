363e876cd0bab41d3e20a2ffb30f6f835cecdacf
Evan Jaramillo <evan.jaramillo@arcfield.com>
2026-05-29 11:57:28 -0500

    updates

--- portfile.cmake
+++ portfile.cmake
@@ -1,30 +1,13 @@
-vcpkg_find_acquire_program(GIT)
-get_filename_component(GIT_EXE_PATH ${GIT} DIRECTORY)
-set(ENV{PATH} "${GIT_EXE_PATH};$ENV{PATH}")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Chlumsky/msdf-atlas-gen
     REF f6a1bc9f76cdaee40a4392e4b4c151cdc959bbb9
-    SHA512 d992258037d113fd3f44b1f5b6511993900b9572c3701aade1ad9af0e9bd7a7939f77f0c7e8a730111eaacbdd5425d5e241ad3f52129bf6639a0b128db895781
+    SHA512 0
     HEAD_REF master
 )
 
-message(STATUS "Fetching msdf-atlas-gen submodules...")
-execute_process(
-        COMMAND ${GIT} submodule update --init --recursive
-        WORKING_DIRECTORY "${SOURCE_PATH}"
-        RESULT_VARIABLE submodule_error
-)
 
-vcpkg_cmake_configure(
-  SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-    -DMSDF_ATLAS_BUILD_STANDALONE=OFF
-    -DMSDF_ATLAS_NO_ARTERY_FONT=ON
-    -DMSDF_ATLAS_MSDFGEN_EXTERNAL=ON
-    -DMSDF_ATLAS_INSTALL=ON
-)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
 
