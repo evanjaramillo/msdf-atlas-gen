3e7a6862da505a254049a4b69be3ce0dabcae694
Evan Jaramillo <evan.jaramillo@arcfield.com>
2026-05-29 12:10:04 -0500

    updates

--- portfile.cmake
+++ portfile.cmake
@@ -5,7 +5,14 @@ vcpkg_from_github(
     SHA512 c6e78ee2689eeb72dff38e4482c0ee360d0a7125d8cb210314adee8f10fd85476a4469d6e78637b9cc213f8b96afbdf13d38673125b702abc71fdf2c200ab57f
     HEAD_REF master
 )
-
+vcpkg_cmake_configure(
+        SOURCE_PATH ${SOURCE_PATH}
+        OPTIONS
+        -DMSDF_ATLAS_BUILD_STANDALONE=OFF
+        -DMSDF_ATLAS_NO_ARTERY_FONT=ON
+        -DMSDF_ATLAS_MSDFGEN_EXTERNAL=ON
+        -DMSDF_ATLAS_INSTALL=ON
+)
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
 
