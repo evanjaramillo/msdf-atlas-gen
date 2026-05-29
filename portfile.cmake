d8fbbe544e48bfee5eeb7608953531e27559f5dd
Evan Jaramillo <evan.jaramillo@arcfield.com>
2026-05-29 12:01:53 -0500

    updates

--- portfile.cmake
+++ portfile.cmake
@@ -1,12 +1,10 @@
-
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Chlumsky/msdf-atlas-gen
-    REF f6a1bc9f76cdaee40a4392e4b4c151cdc959bbb9
-    SHA512 0
+    REF 2ede254314a2512252a225fa6c975948d6af559a
+    SHA512 d992258037d113fd3f44b1f5b6511993900b9572c3701aade1ad9af0e9bd7a7939f77f0c7e8a730111eaacbdd5425d5e241ad3f52129bf6639a0b128db895781
     HEAD_REF master
 )
-
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake)
